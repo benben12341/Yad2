a52b783f4219b0a413c14841862c918dde608c6b
Adi Ben Shushan <adi3765@gmail.com>
2024-03-06 12:07:02 +0200

    Fix ProductDetailsFragment File

--- app/src/main/java/com/example/yad2/fragments/ProductDetailsFragment.kt
+++ app/src/main/java/com/example/yad2/fragments/ProductDetailsFragment.kt
@@ -1,24 +1,23 @@
 package com.example.yad2.fragments
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context
-import android.content.DialogInterface
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
-import android.widget.CompoundButton
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation.findNavController
-import com.example.yad2.interfaces.GetProductByIdListener
+import com.example.yad2.R
+import com.example.yad2.interfaces.AddLikedProductListener
+import com.example.yad2.interfaces.RemoveLikedProductsListener
 import com.example.yad2.models.Model
 import com.example.yad2.models.Product
 import com.example.yad2.models.User
@@ -26,32 +25,30 @@ import com.example.yad2.viewModels.FavoriteProductListRvViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.squareup.picasso.Picasso
 import java.text.MessageFormat
-import com.example.yad2.R
 
-class ProductDetailsFragment() : Fragment() {
-    var imageUrl: ImageView? = null
+open class ProductDetailsFragment : Fragment() {
+    private var imageUrl: ImageView? = null
     var sellerImage: ImageView? = null
-    var title: TextView? = null
-    var price: TextView? = null
-    var gender: TextView? = null
-    var category: TextView? = null
-    var condition: TextView? = null
-    var description: TextView? = null
+    private var title: TextView? = null
+    private var price: TextView? = null
+    private var gender: TextView? = null
+    private var category: TextView? = null
+    private var condition: TextView? = null
+    private var description: TextView? = null
     var sellerName: TextView? = null
-    var editButton: FloatingActionButton? = null
-    var deleteButton: FloatingActionButton? = null
+    private var editButton: FloatingActionButton? = null
+    private var deleteButton: FloatingActionButton? = null
     var progressBar: ProgressBar? = null
     var view: View? = null
-    var removeFromFavorites: ImageView? = null
-    var addToFavorite: ImageView? = null
-    var viewModel: FavoriteProductListRvViewModel? = null
-    var cbIsSold: CheckBox? = null
+    private var removeFromFavorites: ImageView? = null
+    private var addToFavorite: ImageView? = null
+    private var viewModel: FavoriteProductListRvViewModel? = null
+    private var cbIsSold: CheckBox? = null
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        viewModel = ViewModelProvider(this).get(FavoriteProductListRvViewModel::class.java)
+        viewModel = ViewModelProvider(this)[FavoriteProductListRvViewModel::class.java]
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -59,85 +56,95 @@ class ProductDetailsFragment() : Fragment() {
         view = inflater.inflate(R.layout.fragment_product_details, container, false)
         val productId: String = ProductDetailsFragmentArgs.fromBundle(arguments).getProductId()
         attachFragmentElement(view)
-        Model.instance.getProductById(productId, object : GetProductByIdListener {
-            @RequiresApi(api = Build.VERSION_CODES.N)
-            override fun onComplete(product: Product) {
+        Model.instance.getProductById(productId
+        ) { product ->
+            if (product != null) {
                 setFragmentElements(product)
-                displayOwnerButtons(product, productId)
-                getProductSeller(product)
-                FavoritesHandler(product, productId)
             }
-        })
+            if (product != null) {
+                displayOwnerButtons(product, productId)
+            }
+            if (product != null) {
+                getProductSeller(product)
+            }
+            if (product != null) {
+                favoritesHandler(product, productId)
+            }
+        }
         return view
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    private fun FavoritesHandler(product: Product, productId: String) {
+    private fun favoritesHandler(product: Product, productId: String) {
         viewModel?.getData()?.observe(viewLifecycleOwner) { showFavoritesIcon(product) }
         if (!product.contactId?.let { isCurrentUserProduct(it) }!!) {
-            addToFavorite = requireView().findViewById<ImageView>(R.id.add_to_favorite)
-            removeFromFavorites = requireView().findViewById<ImageView>(R.id.in_favorite_icon)
-            HandleRemoveFromFavorites(productId)
-            HandleAddToFavorites(productId)
+            addToFavorite = requireView().findViewById(R.id.add_to_favorite)
+            removeFromFavorites = requireView().findViewById(R.id.in_favorite_icon)
+            handleRemoveFromFavorites(productId)
+            handleAddToFavorites(productId)
         }
     }
 
-    private fun HandleRemoveFromFavorites(productId: String) {
-        removeFromFavorites!!.setOnClickListener(View.OnClickListener {
-            if (removeFromFavorites!!.visibility == View.VISIBLE) {
-                removeFromFavorites!!.visibility = View.GONE
-                addToFavorite!!.visibility = View.VISIBLE
+    private fun handleRemoveFromFavorites(productId: String) {
+        removeFromFavorites?.setOnClickListener {
+            if (removeFromFavorites?.visibility == View.VISIBLE) {
+                removeFromFavorites?.visibility = View.GONE
+                addToFavorite?.visibility = View.VISIBLE
             }
-            Model.instance.removeFromLikedProducts(productId) {}
-        })
+            Model.instance.removeFromLikedProducts(productId, object : RemoveLikedProductsListener {
+                override fun onComplete() {
+                    println("Product removal completed")
+                }
+            })
+        }
     }
 
-    private fun HandleAddToFavorites(productId: String) {
-        addToFavorite!!.setOnClickListener(object : View.OnClickListener {
-            @RequiresApi(api = Build.VERSION_CODES.N)
-            override fun onClick(v: View) {
-                if (addToFavorite!!.visibility == View.VISIBLE) {
-                    removeFromFavorites!!.visibility = View.VISIBLE
-                    addToFavorite!!.visibility = View.GONE
-                }
-                Model.instance.addToLikedProducts(productId) {}
+
+    private fun handleAddToFavorites(productId: String) {
+        addToFavorite?.setOnClickListener {
+            if (addToFavorite?.visibility == View.VISIBLE) {
+                removeFromFavorites?.visibility = View.VISIBLE
+                addToFavorite?.visibility = View.GONE
             }
-        })
+            Model.instance.addToLikedProducts(productId, object : AddLikedProductListener {
+                override fun onComplete() {
+                    println("Product addition to favorites completed")
+                }
+            })
+        }
     }
 
     private fun isCurrentUserProduct(contactId: String): Boolean {
-        return Model.instance.mAuth.getUid().equals(contactId)
+        return Model.instance.mAuth.uid.equals(contactId)
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    private fun getProductSeller(product: Product) {
+    open fun getProductSeller(product: Product) {
         product.contactId?.let {
-            Model.instance.getProductSellerUser(it) { user: User ->
-                progressBar!!.setVisibility(View.GONE)
-                if (user != null) {
+            Model.instance.getProductSellerUser(it, object : Model.GetLoggedUserListener {
+                @SuppressLint("SetTextI18n")
+                override fun onComplete(user: User) {
+                    progressBar?.visibility = View.GONE
                     if (user.userImageUrl != null) {
                         Picasso.get()
                             .load(user.userImageUrl)
                             .into(sellerImage)
                     } else {
-                        sellerImage!!.setImageResource(R.drawable.no_product_image)
+                        sellerImage?.setImageResource(R.drawable.no_product_image)
                     }
-                    sellerName?.setText(user.firstName + " " + user.lastName)
-                    sellerName!!.setOnClickListener(object : View.OnClickListener {
-                        override fun onClick(view: View) {
-                            navigateToUserProfile(user, product.contactId!!)
-                        }
-                    })
-                    sellerImage!!.setOnClickListener(object : View.OnClickListener {
-                        override fun onClick(view: View) {
-                            navigateToUserProfile(user, product.contactId!!)
-                        }
-                    })
-                } else {
-                    sellerImage!!.setVisibility(View.GONE)
-                    sellerName!!.setText("seller not found ):")
+                    sellerName?.text = "${user.firstName} ${user.lastName}"
+                    sellerName?.setOnClickListener {
+                        navigateToUserProfile(
+                            user,
+                            product.contactId!!
+                        )
+                    }
+                    sellerImage?.setOnClickListener {
+                        navigateToUserProfile(
+                            user,
+                            product.contactId!!
+                        )
+                    }
                 }
-            }
+            })
         }
     }
 
@@ -153,48 +160,34 @@ class ProductDetailsFragment() : Fragment() {
 
     private fun displayDeleteButton(product: Product) {
         deleteButton!!.visibility = View.VISIBLE
-        deleteButton!!.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View) {
-                openDeleteDialog(product)
-            }
-        })
+        deleteButton!!.setOnClickListener { openDeleteDialog(product) }
     }
 
     private fun displayCheckIsSoldButton(product: Product) {
         cbIsSold!!.isEnabled = true
-        cbIsSold!!.setOnCheckedChangeListener(object : CompoundButton.OnCheckedChangeListener {
-            override fun onCheckedChanged(buttonView: CompoundButton, isChecked: Boolean) {
-                product.isSold = isChecked
-                Model.instance.saveProduct(product) { isSuccess: Boolean ->
-                    if (isSuccess) {
-                        Log.d("Products", "Product updated successfully")
-                    } else {
-                        Log.d("Products", "Failed to update product")
-                    }
-                }
-            }
-        })
+        cbIsSold!!.setOnCheckedChangeListener { _, isChecked ->
+            product.isSold = isChecked
+            Model.instance.saveProduct(product) { Log.d("Products", "Product updated") }
+        }
     }
 
     private fun displayEditButton(productId: String) {
         editButton!!.visibility = View.VISIBLE
-        editButton!!.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View) {
-                findNavController(v).navigate(
-                    ProductDetailsFragmentDirections.navProductDetailsToNavAddProduct(
-                        productId
-                    )
+        editButton!!.setOnClickListener { v ->
+            findNavController(v).navigate(
+                ProductDetailsFragmentDirections.navProductDetailsToNavAddProduct(
+                    productId
                 )
-            }
-        })
+            )
+        }
     }
 
     private fun setFragmentElements(product: Product) {
-        title?.setText(product.title)
+        title?.text = product.title
         val productPrice: String? = product.price
         price!!.text =
             if (productPrice != null) MessageFormat.format("{0}$", product.price) else ""
-        gender?.setText(product.gender)
+        gender?.text = product.gender
         category?.text  = product.productCategory
         condition?.text = product.productCondition
         description?.text = product.description
@@ -205,37 +198,34 @@ class ProductDetailsFragment() : Fragment() {
     }
 
     private fun attachFragmentElement(view: View?) {
-        imageUrl = requireView().findViewById<ImageView>(R.id.detailsProductImage)
-        title = view?.findViewById<TextView>(R.id.detailsProductTitle)
-        price = view?.findViewById<TextView>(R.id.detailsProductPrice)
-        gender = view?.findViewById<TextView>(R.id.detailProductGender)
-        category = view?.findViewById<TextView>(R.id.detailProductCategory)
-        condition = view?.findViewById<TextView>(R.id.detailsProductCondition)
-        description = view?.findViewById<TextView>(R.id.detailsProductDescription)
-        editButton = view?.findViewById<FloatingActionButton>(R.id.floatingEditButton)
-        deleteButton = view?.findViewById<FloatingActionButton>(R.id.floatingDeleteButton)
-        cbIsSold = view?.findViewById<CheckBox>(R.id.detailsIsSoldCheckBox)
-        sellerImage = view?.findViewById<ImageView>(R.id.seller_image)
-        sellerName = view?.findViewById<TextView>(R.id.seller_name)
-        progressBar = view?.findViewById<ProgressBar>(R.id.products_details_progress_bar)
-        addToFavorite = view?.findViewById<ImageView>(R.id.add_to_favorite)
-        removeFromFavorites = view?.findViewById<ImageView>(R.id.in_favorite_icon)
+        imageUrl = requireView().findViewById(R.id.detailsProductImage)
+        title = view?.findViewById(R.id.detailsProductTitle)
+        price = view?.findViewById(R.id.detailsProductPrice)
+        gender = view?.findViewById(R.id.detailProductGender)
+        category = view?.findViewById(R.id.detailProductCategory)
+        condition = view?.findViewById(R.id.detailsProductCondition)
+        description = view?.findViewById(R.id.detailsProductDescription)
+        editButton = view?.findViewById(R.id.floatingEditButton)
+        deleteButton = view?.findViewById(R.id.floatingDeleteButton)
+        cbIsSold = view?.findViewById(R.id.detailsIsSoldCheckBox)
+        sellerImage = view?.findViewById(R.id.seller_image)
+        sellerName = view?.findViewById(R.id.seller_name)
+        progressBar = view?.findViewById(R.id.products_details_progress_bar)
+        addToFavorite = view?.findViewById(R.id.add_to_favorite)
+        removeFromFavorites = view?.findViewById(R.id.in_favorite_icon)
     }
 
     private fun openDeleteDialog(product: Product) {
         val alert = AlertDialog.Builder(activity)
         alert.setTitle("Do you want to delete this product?")
-        alert.setPositiveButton("Yes", object : DialogInterface.OnClickListener {
-            override fun onClick(dialog: DialogInterface, whichButton: Int) {
-                product.isDeleted = true
-                Model.instance.saveProduct(product) {
-                    findNavController((title)!!).navigateUp() }
+        alert.setPositiveButton("Yes"
+        ) { _, _ ->
+            product.isDeleted = true
+            Model.instance.saveProduct(product) {
+                findNavController((title)!!).navigateUp()
             }
-        })
-        alert.setNegativeButton("Cancel",
-            object : DialogInterface.OnClickListener {
-                override fun onClick(dialog: DialogInterface, whichButton: Int) {}
-            })
+        }
+        alert.setNegativeButton("Cancel") { _, _ -> }
         alert.show()
     }
 
@@ -244,14 +234,13 @@ class ProductDetailsFragment() : Fragment() {
         bundle.putSerializable("user", user)
         val userProfileFragment = UserProfileFragment()
         userProfileFragment.arguments = bundle
-        if (Model.instance.mAuth.getUid().equals(contactId)) {
-            findNavController((view)!!).navigate(R.id.to_nav_user_profile, bundle)
+        if (Model.instance.mAuth.uid.equals(contactId)) {
+            findNavController(requireView()).navigate(R.id.to_nav_user_profile, bundle)
         } else {
-            findNavController((view)!!).navigate(R.id.to_other_user_profile, bundle)
+            findNavController(requireView()).navigate(R.id.to_other_user_profile, bundle)
         }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
     private fun showFavoritesIcon(product: Product) {
         if (!product.contactId?.let { isCurrentUserProduct(it) }!!) {
             val favoritesProducts: List<String> = viewModel?.getData()?.value
